7c3db76838d3d1bf78a5e759525e4a7077891427
yilber muñoz <yilberg364@gmail.com>
2024-08-15 15:41:03 -0500

    buuu

--- enviar_ayuda.php
+++ enviar_ayuda.php
@@ -1,48 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+</html>
+
 <?php
 include ("config/conexion.php"); // Asegúrate de que este archivo esté en la misma ubicación que config.php
 
 // Asumo que el usuario está registrado y tienes su ID en una variable de sesión
 session_start();
-$id_usuario =  $_SESSION['id_usuario']; // Ajusta esto según cómo manejes las sesiones
+$id_usuario = $_SESSION['id_usuario']; // Ajusta esto según cómo manejes las sesiones
 
-
-
+$alertType = ''; // Variable para almacenar el tipo de alerta
+$alertMessage = ''; // Variable para almacenar el mensaje de la alerta
 
 if (
-    $_POST['name'] =='' ||
-    $_POST['email']=='' ||
+    $_POST['name'] == '' ||
+    $_POST['email'] == '' ||
     $_POST['message'] == ''
-
-
 ) {
-    echo '<script>
-        alert("Debes completar todos los campos");
-        location.href = ("ayuda.php");        
- </script>';
+    $alertType = 'error';
+    $alertMessage = 'Debes completar todos los campos';
 } else {
-    $id_usuario =  $_SESSION['id_usuario']; // Ajusta esto según cómo manejes las sesiones
-
     $nombre = $_POST['name'];
     $correo = $_POST['email'];
     $mensaje = $_POST['message'];
     
-    //ejecutar la consulta de update
-
-    $query_INSERT = "INSERT INTO d_ayuda (id_usuario, nombre, correo, mensaje) VALUES ( '$id_usuario','$nombre', '$correo', '$mensaje') ";
-                            
-
-    $execute_INSERT= mysqli_query($conn, $query_INSERT) or die(mysqli_error($conn));
+    // Ejecutar la consulta de inserción
+    $query_INSERT = "INSERT INTO d_ayuda (id_usuario, nombre, correo, mensaje) VALUES ('$id_usuario', '$nombre', '$correo', '$mensaje')";
+    $execute_INSERT = mysqli_query($conn, $query_INSERT);
 
     if ($execute_INSERT) {
-        echo '<script>
-                    alert("AYUDA ENVIADA");
-                    location.href = ("ayuda.php");        
-             </script>';
+        $alertType = 'success';
+        $alertMessage = 'AYUDA ENVIADA';
     } else {
-        echo '<script>
-                    alert("Lo siento algo salió mal");
-                    location.href = ("ayuda.php");        
-             </script>';
+        $alertType = 'error';
+        $alertMessage = 'Lo siento, algo salió mal';
     }
 }
 
+echo "<script>
+    Swal.fire({
+        icon: '$alertType',
+        title: '$alertMessage',
+        confirmButtonText: 'Aceptar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = 'ayuda.php';
+        }
+    });
+</script>";
+?>
